d38ac5fad7e996af5bf86d09281b5a0adc63289d
hansen hansen and sravanthi velaga <sravanthi@>
2017-11-29 11:44:33 -0800

    final commit

--- build/js/app.js
+++ build/js/app.js
@@ -1,18 +1,18 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 $(document).ready(function () {
-  $('#searchBike').submit(function (event) {
-    $("#result").text("");
+  $('#dinoButton').click(function (event) {
+    $('#result').text("");
     event.preventDefault();
-    var title = $('.title').val();
-    var location = $('.location').val();
-    $('.title').val("");
-    $('.location').val("");
+    var paragraph = $('.paragraph').val();
+    var words = $('.words').val();
+    $('.paragraph').val("");
+    $('.words').val("");
 
     var promise = new Promise(function (resolve, reject) {
       var request = new XMLHttpRequest();
-      var url = "https://bikeindex.org/api/v3/search?page=1&per_page=25&manufacturer=" + title + "&location=" + location + "&stolenness=stolen";
+      var url = 'http://dinoipsum.herokuapp.com/api/?format=json&paragraphs=' + paragraph + '&words=' + words;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -26,14 +26,14 @@ $(document).ready(function () {
 
     promise.then(function (response) {
       var body = JSON.parse(response);
-      body.bikes.forEach(function (bike) {
-        // date in UNIX timestamp(formula to convert is =(((givenNumber)/60)/60)/24)+DATE(1970,1,1);
-        if (bike.date_stolen >= 1511308800 && bike.date_stolen <= 1511848800) {
-          $('#result').append("Bike ID: " + bike.id + "<br>" + ("Bike title: " + bike.title) + "<br>");
-        }
+      body.forEach(function (sentence) {
+        sentence.forEach(function (word) {
+          $('#result').append(word + " ");
+        });
+        $('#result').append("<br>");
       });
     }, function (error) {
-      $('.showErrors').text("There was an error processing your request: " + error.message);
+      $('.showErrors').text('There was an error processing your request: ' + error.message);
     });
   });
 });
